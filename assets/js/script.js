b42a6eba99fd6d8f6d0b3985510ea9f699d73eaa
Gustavo98Soares <gustavo_soares98@outlook.com>
2022-03-17 18:19:27 -0300

    First commit

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+var typed = new Typed(".typedJs", {
+    strings: ['console.log("Hello World")'],
+    typeSpeed:60,
+    backSpeed:60,
+  });
+
+var typed = new Typed(".typedHtml", {
+    strings: ['&lth1&gt Olá!!! &lt/h1&gt '],
+    typeSpeed:60,
+  });
+
+  var typed = new Typed(".typedCss", {
+    strings: ['.container {color: red; }'],
+    typeSpeed:60,
+    backSpeed:60,
+  });
+
