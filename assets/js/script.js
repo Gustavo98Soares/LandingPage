8b4adcc54e68f2261091f85b4f2b69d59cb39685
Gustavo98Soares <gustavo_soares98@outlook.com>
2022-03-18 15:45:44 -0300

    Atualização

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,16 +2,20 @@ var typed = new Typed(".typedJs", {
     strings: ['console.log("Hello World")'],
     typeSpeed:60,
     backSpeed:60,
+    loop:true,
   });
 
 var typed = new Typed(".typedHtml", {
     strings: ['&lth1&gt Olá!!! &lt/h1&gt '],
     typeSpeed:60,
+    backSpeed:60,
+    loop:true,
   });
 
   var typed = new Typed(".typedCss", {
     strings: ['.container {color: red; }'],
     typeSpeed:60,
     backSpeed:60,
+    loop:true,
   });
 
