e63addd567be1c8e17541da35596c80afc1d3548
Gustavo98Soares <gustavo_soares98@outlook.com>
2022-07-19 22:30:16 -0300

    Continuidade do projeto

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,25 @@
+const button = document.querySelector('.button-theme')
+if (button){
+  button.addEventListener('click',event =>{
+    event.preventDefault()
+    document.body.setAttribute('data-theme','t-playstation')
+  })
+}
+
 
 /*-------------------------------- Typed------------------------------------------------*/
-var typed = new Typed(".typedJs", {
-    strings: ['console.log("Hello World")'],
-    typeSpeed:60,
-    backSpeed:60,
-    loop:true,
-  });
-
-var typed = new Typed(".typedHtml", {
-    strings: ['&lth1&gt Olá!!! &lt/h1&gt '],
-    typeSpeed:60,
-    backSpeed:60,
-    loop:true,
-  });
-
-  var typed = new Typed(".typedCss", {
-    strings: ['.container {color: red; }'],
-    typeSpeed:60,
-    backSpeed:60,
-    loop:true,
-  });
+var typed = new Typed(".typedMyName", {
+  strings: [' Gustavo'],
+  typeSpeed:150,
+  backSpeed:200,
+  loop:true,
+});
 
 /*----------------------------------- Owl -------------------------------------------*/
 $('.certificates__owl').owlCarousel({
   loop:true,
   autoplay:true,
-  autoplayTimeout:2000,
+  autoplayTimeout:5000,
   autoplayHoverPause:true,
   center: true,
   margin:10,
